cae53c7ec2217b362cfd95e5a8d501f2026ca44c
Doris940704 <146065147+Doris940704@users.noreply.github.com>
2023-10-31 14:07:35 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -8,12 +8,16 @@ double month_pay=0;
 scanf("%d%d",&work_time,&hr_pay);
 if(work_time<=60)
    month_pay=work_time*hr_pay;
-if(work_time>60&&work_time<=120)
-   month_pay=work_time*hr_pay*1.33;
-if(work_time>120)
-   month_pay=work_time*hr_pay*1.66;
-
+if(work_time>60&&work_time<=120){
+   int work_time1=0;
+   work_time1=work_time-60;
+   month_pay=work_time1*hr_pay*1.33+60*hr_pay;
+}
+if(work_time>120){
+   int work_time2=0;
+   work_time2=work_time-120;
+   month_pay=work_time2*hr_pay*1.66+60*1.33*hr_pay+60*hr_pay;
+}
 printf("%.1f",month_pay);
 
 }
-
