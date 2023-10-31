2d37582eda414d94cee89050065a76a22a78f97f
Doris940704 <146065147+Doris940704@users.noreply.github.com>
2023-10-31 13:42:04 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int call_min=0;
+double call_pay=0;
+scanf("%d",&call_min);
+if(call_min<=800)
+   call_pay=0.9*call_min;
+if(call_min>800&&call_min<1500)
+   call_pay=(0.9*call_min)*0.9;
+if(call_min>=1500)
+   call_pay=(0.9*call_min)*0.79;
+
+printf("%.1f",call_pay);
+
+}
 
