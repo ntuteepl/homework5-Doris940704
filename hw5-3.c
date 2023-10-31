f88e1d73357947996e5990842d04a20c43127dd4
Doris940704 <146065147+Doris940704@users.noreply.github.com>
2023-10-31 13:54:20 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int work_time=0,hr_pay=0;
+double month_pay=0;
+scanf("%d%d",&work_time,&hr_pay);
+if(work_time<=60)
+   month_pay=work_time*hr_pay;
+if(work_time>60&&work_time<=120)
+   month_pay=work_time*hr_pay*1.33;
+if(work_time>120)
+   month_pay=work_time*hr_pay*1.66;
+
+printf("%.1f",month_pay);
+
+}
 
